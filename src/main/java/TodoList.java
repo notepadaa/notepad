4f65328d7f783bee6ec533ce83b89c44e5ee0617
Git-xht <1443921468@qq.com>
2020-06-28 14:47:57 +0800

    有关添加功能的代码

---
+++ src/main/java/TodoList.java
@@ -0,0 +1,39 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class TodoList {
+    private ArrayList<Todo> todoList;
+    private int listSize;               //Capacity of todolist
+
+    public TodoList(){
+        this.todoList = new ArrayList<Todo>();
+        this.listSize = 0;
+    }
+    public TodoList(int size){
+        this.todoList = new ArrayList<Todo>();
+        this.listSize = size;
+    }
+
+    public int addTodo(Todo todo){
+        if(todo.getTitle() == null){
+            System.err.println("Todo title must not be null.");
+            return -2;
+        }
+        if(this.todoList.size() == this.listSize){
+            System.err.println("Failed, list is full.");
+            return -1;
+        }
+        todoList.add(todo);
+        return 1;
+    }
+
+    public int finishTodo(String todoTitle){
+        for(Todo todo : todoList)
+            if(todo.getTitle().equals(todoTitle)){
+                todo.finish();
+                return 1;
+            }
+        System.err.println("Todo not exists in list.");
+        return -1;
+    }
+}
